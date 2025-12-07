0f7592875e60462654b6694915965d0ff9b232f6
Dimitar Klianev <mitko6754@gmail.com>
2025-12-07 16:38:19 +0200

    Add custom exception classes for error handling

---
+++ src/main/java/org/nbu/transport/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package org.nbu.transport.exception;
+
+public class ValidationException extends TransportException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
